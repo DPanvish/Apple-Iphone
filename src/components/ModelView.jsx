d8130e5c638b2773c49e4edb083ef865fc12c0d2
Panvish Dowripilli <panvishd@gmail.com>
2025-08-28 02:44:36 +0530

    IPhone model is added to the view

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,17 +1,18 @@
-import { PerspectiveCamera, View } from "@react-three/drei"
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import Lights from "./Lights"
 import IPhone from "./IPhone"
 import { Suspense } from "react"
+import * as THREE from "three"
 
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
   
 
   return (
     <View 
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${index === 2} ? right-[-100%] : ""`}
+      className={`w-full h-full ${index === 2} ? right-[-100%] : ""`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
@@ -22,10 +23,33 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size
       {/* Lights for lighting the scene */}
       <Lights />
 
-      {/* Suspense for loading model */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <IPhone />
-      </Suspense>
+      {/* Controls for rotating and zooming in/out of the scene */}
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      {/* 
+        Group Ref is used to rotate the phone 
+        and it's children elements.
+        It also has a name which is used to identify the group.
+        Based on name we can set either samll or large model
+      */}
+      <group ref={groupRef} name={`${index === 1} ? small : large`} position={[0, 0, 0]}>
+        {/* Suspense is used to load the model asynchronously */}
+        <Suspense fallback={<Html><div>Loading...</div></Html>}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   )
 }
