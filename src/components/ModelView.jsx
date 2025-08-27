fec625984200f1c66943ad6d4e2233a8e9ea4146
Panvish Dowripilli <panvishd@gmail.com>
2025-08-28 02:29:28 +0530

    IPhone model created

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,32 @@
+import { PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import IPhone from "./IPhone"
+import { Suspense } from "react"
 
 
-const ModelView = () => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   
 
   return (
-    <div>ModelView</div>
+    <View 
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${index === 2} ? right-[-100%] : ""`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      {/* Perspective Canera similar to human eye */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      {/* Lights for lighting the scene */}
+      <Lights />
+
+      {/* Suspense for loading model */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <IPhone />
+      </Suspense>
+    </View>
   )
 }
 
