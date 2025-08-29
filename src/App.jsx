05bd819f1fb11fe0dfbf14006d865bf45239a260
Panvish Dowripilli <panvishd@gmail.com>
2025-08-30 02:28:41 +0530

    Sentry Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
+import * as Sentry from "@sentry/react"
+
+// We are using sentry to track the performance of the site.
+// Follow all instructions whilw setting up the sentry sdk
+// The sentry setup is in the main.jsx
+
 
 const App = () => {
 
@@ -15,4 +21,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a1e30ffccf95bd58fa16df427fec211f@o4509929247408128.ingest.us.sentry.io/4509929249374208",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    })
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+  sendDefaultPii: true
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
